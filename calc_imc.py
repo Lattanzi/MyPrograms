1f13e2d577e9e29fc5af307897d1a851f6826fea
Lattanzi <lucas_latta@hotmail.com>
2021-06-28 17:59:41 -0300

    Add files via upload

---
+++ calc_imc.py
@@ -0,0 +1,40 @@
+
+
+def calc_imc(peso, altura):
+    print('Calculando o imc...')
+    valor_imc = peso/(altura*altura)
+    return(valor_imc)
+
+def classe_imc(sexo,peso,altura):
+
+    print('Classificando o imc...')
+
+    IMC = calc_imc(peso,altura)
+
+    if sexo == 'm':
+        if IMC < 20.7:
+            return 'Abaixo do peso!', 'IMC:', IMC
+        elif IMC >= 20.7 and IMC <= 26.4:
+            return 'No peso normal!', 'IMC:', IMC
+        elif IMC > 26.4 and IMC <= 27.8:
+            return 'Marginalmente acima do peso!', 'IMC:', IMC
+        elif IMC > 27.8 and IMC <= 31.1:
+            return 'Acima do peso ideal!', 'IMC:', IMC
+        elif IMC > 31.1:
+            return 'Obeso!', 'IMC:', IMC
+        else:
+            return '[ERROR]Erro na classificação do imc!'
+
+    else:
+        if IMC < 19.1:
+            return '\n Abaixo do peso!', 'IMC:', IMC
+        elif IMC >= 19.1 and IMC <= 25.8:
+            return 'No peso normal!', 'IMC:', IMC
+        elif IMC > 25.8 and IMC <= 27.3:
+            return 'Marginalmente acima do peso!', 'IMC:', IMC
+        elif IMC > 27.3 and IMC <= 32.3:
+            return 'Acima do peso ideal!', 'IMC:', IMC
+        elif IMC > 32.3:
+            return 'Obeso!', 'IMC:', IMC
+        else:
+            return '[ERROR]Erro na classificação do imc!'
