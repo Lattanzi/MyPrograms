e8e812ec83ebd3fef823298b2c07feb9accab64b
Lattanzi <lucas_latta@hotmail.com>
2021-06-29 08:18:28 -0300

    Add files via upload

---
+++ calc_imc.py
@@ -0,0 +1,6 @@
+
+
+def calc_imc(peso, altura):
+    print('Calculando o imc...')
+    valor_imc = peso/(altura*altura)
+    return(valor_imc)
---
+++ dados_imc.py
@@ -0,0 +1,50 @@
+import classe_imc
+
+
+print('Gerando e validando os dados para calculo de imc...')
+
+verif = 's'
+valid = False
+
+while verif == 's':
+
+    while valid == False:
+        sexo = input('Entre com o sexo(M/F): ').lower()
+        if sexo == 'm' or sexo == 'f':
+            valid = True
+        else:
+            print('Sexo inválido!')
+
+    valid = False
+
+    while valid == False:
+        peso = input('Entre com o peso: ')
+        try:
+            peso = float(peso)
+            if peso > 0 and peso < 400:
+                valid = True
+            else:
+                print('Entre com o peso de um humano!')
+        except:
+            print('Valor do peso inválido!')
+
+    valid = False
+
+    while valid == False:
+        altura = input('Entre com a altura: ')
+        try:
+            altura = float(altura)
+            if altura > 0 and altura < 3:
+                valid = True
+            else:
+                print('Entre com a altura de um humano!')
+        except:
+            print('Valor da altura inválido!')
+
+    valid = False
+
+    print('\n Sexo:', sexo, 'Peso:', peso, 'Altura:', altura)
+
+    print(classe_imc.classe_imc(sexo,peso,altura))
+
+    verif = input("Deseja verificar mais imc's(S/N)?")
